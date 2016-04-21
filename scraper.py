9ba8c22136889f6e43b29af5f4ede0faa735cc84
Ian Ibbotson <ianibbo@gmail.com>
2016-04-21 15:33:36 +0100

    Working on sherpa fact scraper

--- scraper.py
+++ scraper.py
@@ -1,15 +1,30 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
+import scraperwiki
+import lxml.html
+
 #
 # # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
+html = scraperwiki.scrape("http://www.sherpa.ac.uk/juliet/index.php?la=en&mode=simple&page=browse")
 #
 # # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
+root = lxml.html.fromstring(html)
+
+print 'Process'
+
+for row in root.cssselect("table.juliettable tr"):
+    td_elements = row.cssselect("td")
+    if td_elements is not None:
+        if len(td_elements) > 1:
+            a_elements = td_elements[0].cssselect("a")
+            if len(a_elements) == 2:
+              print a_elements[0].attrib["name"], a_elements[1].text, a_elements[1].attrib["href"]
+    else:
+        print "Missing"
+
+    
+
 #
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
